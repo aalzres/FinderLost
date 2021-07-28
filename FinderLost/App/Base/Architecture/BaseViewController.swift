9a9fa6259f827a914f67c5c4dc7d858f954dfbf0
Andrés Felipe <aalzres@gmail.com>
2021-07-28 23:40:28 +0200

    Create base

---
+++ FinderLost/App/Base/Architecture/BaseViewController.swift
@@ -0,0 +1,12 @@
+//
+//  BaseViewController.swift
+//  FinderLost
+//
+//  Created by Andres Felipe Alzate Restrepo on 28/7/21.
+//
+
+import SwiftUI
+
+protocol BaseViewController: AnyObject { }
+
+open class BaseViewControllerImpl: UIViewController, BaseViewController { }
