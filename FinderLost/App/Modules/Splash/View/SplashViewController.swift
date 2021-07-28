5b301a5c2b4c10d91f26c23c7cbc291a794e8750
Andrés Felipe <aalzres@gmail.com>
2021-07-29 00:31:21 +0200

    Implemented SplashViewController with UIKit

---
+++ FinderLost/App/Modules/Splash/View/SplashViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SplashViewController.swift
+//  FinderLost
+//
+//  Created by Andres Felipe Alzate Restrepo on 28/7/21.
+//
+
+import SwiftUI
+
+final class SplashViewControllerImpl: BaseViewControllerImpl {
+    lazy var splashImageView = UIImageView()
+        .set(\.image, splashImage)
+    
+    lazy var splashImage = UIImage(named: "splash_logo")
+
+    override func setupView() {
+        super.setupView()
+
+        view.addSubview(splashImageView)
+        splashImageView.translatesAutoresizingMaskIntoConstraints = false
+        splashImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        splashImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+}
